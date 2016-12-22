0f4aa29304e2d94617ef1cf27dd1761995666f15
Rayson Zhu <vfreex+git@gmail.com>
2016-12-22 18:12:15 +0800

    add initial code

--- include/frul.h
+++ include/frul.h
@@ -2,6 +2,7 @@
 #define _FRUL_H
 
 #include <stdint.h>
+#include <stdbool.h>
 #include "endian.h"
 #include "list.h"
 
@@ -12,7 +13,7 @@ typedef uint64_t __be64;
 struct frul_hdr {
     char  ver;
     char reserved;
-#if  __BIG_ENDIAN
+#if  __LITTLE_ENDIAN
     __be16 f_res1: 8,
            f_init: 1,
            f_ack: 1,
@@ -21,7 +22,7 @@ struct frul_hdr {
            f_comp: 1,
            f_opt: 1,
            f_res2: 2;
-#elif __LITTLE_ENDIAN
+#elif __BIG_ENDIAN
      __be16 f_res1: 8,
            f_res2: 2,
            f_init: 1,
@@ -41,28 +42,72 @@ struct frul_hdr {
     __be32 options[];
 };
 
+enum frul_state {
+    FRUL_CLOSED,
+    FRUL_LISTEN,
+    FRUL_INIT_SENT,
+    FRUL_INIT_ACKED,
+    FRUL_CONN_SENT,
+    FRUL_CONN_RECV,
+    FRUL_ESTABLISHED,
+    FRUL_SHUT_SENT,
+    FRUL_SHUT_RECV,
+    FRUL_COMP_WAIT,
+    FRUL_COMPLETING,
+    FRUL_TIME_WAIT
+};
+
+enum frul_errno {
+    FRUL_E_SUCCESS,
+    FRUL_E_UNKNOWN,
+    FRUL_E_STATE,
+    FRUL_E_NOMEM,
+    FRUL_E_RANGE,
+    FRUL_E_UNSUPP,
+    FRUL_E_NOTIMPL,
+    FRUL_E_WOULDBLOCK,
+    FRUL_E_IO,
+    FRUL_E_WOULDFRAG
+};
+
 struct frulcb {
-    int (*output)(const char *buffer, size_t n, void *userdata);
+    enum frul_state state;
+    enum frul_errno errno;
     size_t send_next;
     size_t recv_next;
     size_t cwnd;
     size_t rwnd;
     size_t send_capacity;
     size_t recv_capacity;
+    size_t mss;
+    void *userdata;
+    ssize_t (*output)(const void *buffer, size_t n, void *userdata);
 
+    size_t write_buffer_used;
+    size_t write_buffer_limit;
+    size_t read_buffer_used;
+    size_t read_buffer_limit;
     struct list_head write_queue;
     struct list_head *send_head;
 };
 
-struct frul_buff {
+struct frul_buf {
     struct list_head list;
     size_t data_len;
     char data[];
 };
 
+#define RRUL_PROTO_VERSION 1
+#define FRUL_DEFAULT_MSS 1400
+#define FRUL_MAX_RECV_BUFFER 100000
+#define FRUL_MAX_SEND_BUFFER 100000
+#define FRUL_INITIAL_CWND 2
+
 int frul_input(struct frulcb *frul, char *buffer, size_t n);
 
 int frul_init(struct frulcb *frul);
+
+int frul_listen(struct frulcb *frul, bool listen);
 
 int frul_send(struct frulcb *frul, const char *buffer, size_t n);
 
--- src/frul.c
+++ src/frul.c
@@ -1,15 +1,105 @@
 #include <string.h>
+#include <assert.h>
+#include <stdlib.h>
 
+#include <debug.h>
 #include <frul.h>
+
 
 int frul_init(struct frulcb *frul)
 {
     memset(frul, 0, sizeof(struct frulcb));
-    return -1;
+    INIT_LIST_HEAD(&frul->write_queue);
+    frul->state = FRUL_CLOSED;
+    frul->mss = FRUL_DEFAULT_MSS;
+    frul->read_buffer_limit = FRUL_MAX_RECV_BUFFER;
+    frul->write_buffer_limit = FRUL_MAX_SEND_BUFFER;
+    return 0;
+}
+
+int frul_listen(struct frulcb *frul, bool listen)
+{
+    if (frul->state != FRUL_CLOSED && frul->state != FRUL_LISTEN) {
+        frul->errno = FRUL_E_STATE;
+        return -1;
+    }
+    frul->state = listen ? FRUL_LISTEN : FRUL_CLOSED;
+    frul->errno = FRUL_E_SUCCESS;
+    return 0;
+}
+
+static int frul_put_write_queue(struct frulcb *frul, struct frul_buf *buf)
+{
+    size_t write_buffer_used = frul->write_buffer_used + buf->data_len;
+    if (write_buffer_used > frul->write_buffer_limit) {
+        frul->errno = FRUL_E_WOULDBLOCK;
+        return -1;
+    }
+    list_add_tail(&buf->list, &frul->write_queue);
+    return 0;
+}
+
+static inline struct frul_hdr *frul_seg_hdr(struct frul_buf *buf)
+{
+    return (struct frul_hdr *)((char *)buf + offsetof(struct frul_buf, data));
+}
+
+static inline struct frul_buf *frul_buf_new(size_t data_len)
+{
+    size_t seg_size = sizeof(struct frul_buf) + sizeof(struct frul_hdr) + data_len;
+    struct frul_buf *buf = calloc(seg_size, 1);
+    if (buf)
+        buf->data_len = data_len;
+    return buf;
+}
+
+int frul_flush(struct frulcb *frul)
+{
+    struct frul_buf *buf, *tempbuf;
+    ssize_t bytes_sent;
+    list_for_each_entry_safe(buf, tempbuf, &buf->list, list) {
+        bytes_sent = frul->output(buf->data, buf->data_len, frul->userdata);
+        if (bytes_sent < 0) {
+            frul->errno = FRUL_E_IO;
+            return -1;
+        }
+        else if (bytes_sent != buf->data_len) {
+            frul->errno = FRUL_E_WOULDFRAG;
+            return -1;
+        }
+        list_del(&buf->list);
+    }
+    return 0;
+}
+
+int frul_connect(struct frulcb *frul)
+{
+    if (frul->state != FRUL_CLOSED) {
+        frul->errno = FRUL_E_STATE;
+        return -1;
+    }
+    struct frul_buf *buf = frul_buf_new(0);
+    if (!buf) {
+        frul->errno = FRUL_E_NOMEM;
+        return -1;
+    }
+    struct frul_hdr *hdr = frul_seg_hdr(buf);
+    hdr->ver = RRUL_PROTO_VERSION;
+    hdr->f_init = 1;
+    if (frul_put_write_queue(frul, buf) == -1) {
+        return -1;
+    }
+    if (frul_flush(frul) == -1) {
+        return -1;
+    }
+    return 0;
+    
 }
 
 int frul_send(struct frulcb *frul, const char *buffer, size_t n)
 {
+    if (frul->state == FRUL_CLOSED) {
+    }
     return -1;
 }
 
@@ -18,7 +108,3 @@ int frul_recv(struct frulcb *frul, char *buffer, size_t n)
     return -1;
 }
 
-int frul_flush(struct frulcb *frul)
-{
-    return -1;
-}
