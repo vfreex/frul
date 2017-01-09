e2871af97ac2d9edb3175af03f2bf5fbbb0aed81
Rayson Zhu <vfreex+git@gmail.com>
2017-01-09 17:06:12 +0800

    add common.h

---
+++ include/common.h
@@ -0,0 +1,14 @@
+#ifndef FRUL_COMMON_H
+#define FRUL_COMMON_H
+
+#ifndef max
+#define max(a, b) \
+    ( (a) > (b) ? (a) : (b) )
+#endif
+
+#ifndef min
+#define min(a, b) \
+    ( (a) < (b) ? (a) : (b) )
+#endif
+
+#endif //FRUL_COMMON_H
