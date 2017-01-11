bb5fbefea66d858090bc1e7b003c6dae93adec3e
Rayson Zhu <vfreex+git@gmail.com>
2017-01-11 16:25:50 +0800

    revise states

--- include/frul.h
+++ include/frul.h
@@ -11,90 +11,86 @@ typedef uint32_t __be32;
 typedef uint64_t __be64;
 
 struct frul_hdr {
-    char  ver;
-    char reserved;
+  char ver;
+  char reserved;
 #if  __LITTLE_ENDIAN
-    __be16 f_res1: 8,
-           f_init: 1,
-           f_ack: 1,
-           f_conn: 1,
-           f_shut: 1,
-           f_comp: 1,
-           f_opt: 1,
-           f_res2: 2;
+  __be16 f_res1: 8,
+      f_init: 1,
+      f_ack: 1,
+      f_conn: 1,
+      f_shut: 1,
+      f_comp: 1,
+      f_opt: 1,
+      f_res2: 2;
 #elif __BIG_ENDIAN
-     __be16 f_res1: 8,
-           f_res2: 2,
-           f_init: 1,
-           f_comp: 1,
-           f_shut: 1,
-           f_conn: 1,
-           f_ack: 1,
-           f_opt: 1;
+  __be16 f_res1: 8,
+        f_res2: 2,
+        f_init: 1,
+        f_comp: 1,
+        f_shut: 1,
+        f_conn: 1,
+        f_ack: 1,
+        f_opt: 1;
 #else
 #error "Could not determine byte order."
 #endif
-    __be32 seq;
-    __be32 ack_seq;
-    __be32 window;
-    __be16 ts;
-    __be16 ts_echo;
-    __be32 options[];
+  __be32 seq;
+  __be32 ack_seq;
+  __be32 window;
+  __be16 ts;
+  __be16 ts_echo;
+  __be32 options[];
 };
 
 enum frul_state {
-    FRUL_CLOSED,
-    FRUL_LISTEN,
-    FRUL_INIT_SENT,
-    FRUL_INIT_ACKED,
-    FRUL_CONN_SENT,
-    FRUL_CONN_RECV,
-    FRUL_ESTABLISHED,
-    FRUL_SHUT_SENT,
-    FRUL_SHUT_RECV,
-    FRUL_COMP_WAIT,
-    FRUL_COMPLETING,
-    FRUL_TIME_WAIT
+  FRUL_CLOSED,
+  FRUL_LISTEN,
+  FRUL_INIT_SENT,
+  FRUL_CONN_SENT,
+  FRUL_ESTABLISHED,
+  FRUL_SHUT_SENT,
+  FRUL_LAST_ACK,
+  FRUL_TIME_WAIT
 };
 
 enum frul_errno {
-    FRUL_E_SUCCESS,
-    FRUL_E_UNKNOWN,
-    FRUL_E_STATE,
-    FRUL_E_NOMEM,
-    FRUL_E_RANGE,
-    FRUL_E_UNSUPP,
-    FRUL_E_NOTIMPL,
-    FRUL_E_WOULDBLOCK,
-    FRUL_E_IO,
-    FRUL_E_WOULDFRAG
+  FRUL_E_SUCCESS,
+  FRUL_E_UNKNOWN,
+  FRUL_E_STATE,
+  FRUL_E_NOMEM,
+  FRUL_E_RANGE,
+  FRUL_E_UNSUPP,
+  FRUL_E_NOTIMPL,
+  FRUL_E_WOULDBLOCK,
+  FRUL_E_IO,
+  FRUL_E_WOULDFRAG
 };
 
 struct frulcb {
-    enum frul_state state;
-    enum frul_errno errno;
-    size_t send_next;
-    size_t recv_next;
-    size_t cwnd;
-    size_t rwnd;
-    size_t send_capacity;
-    size_t recv_capacity;
-    size_t mss;
-    void *userdata;
-    ssize_t (*output)(const void *buffer, size_t n, void *userdata);
+  enum frul_state state;
+  enum frul_errno errno;
+  size_t send_next;
+  size_t recv_next;
+  size_t cwnd;
+  size_t rwnd;
+  size_t send_capacity;
+  size_t recv_capacity;
+  size_t mss;
+  void *userdata;
+  ssize_t (*output)(const void *buffer, size_t n, void *userdata);
 
-    size_t write_buffer_used;
-    size_t write_buffer_limit;
-    size_t read_buffer_used;
-    size_t read_buffer_limit;
-    struct list_head write_queue;
-    struct list_head *send_head;
+  size_t write_buffer_used;
+  size_t write_buffer_limit;
+  size_t read_buffer_used;
+  size_t read_buffer_limit;
+  struct list_head write_queue;
+  struct list_head *send_head;
 };
 
 struct frul_buf {
-    struct list_head list;
-    size_t data_len;
-    char data[];
+  struct list_head list;
+  size_t data_len;
+  char data[];
 };
 
 #define FRUL_PROTO_VERSION 1
